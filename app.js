eb76a87c440846c8c340991bbe97313ed603b90a
Sumit Jaiswal <sj0855530@gmail.com>
2024-10-19 14:58:56 +0530

    app changes

--- app.js
+++ app.js
@@ -1,71 +1,69 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Program = require("./models/programSchema.js")
-const MONGO_URL = 'mongodb://127.0.0.1:27017/innerbhakti'
+const Program = require("./models/programSchema.js");
+const MONGO_URL = process.env.MONGO_URL;
 const ejsMate = require("ejs-mate");
 
-main().then(() => {
-    console.log("connected to db")
-}).catch((err) =>{
-    console.log(err);
-});
-
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("Connected to DB");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // Exit on error
+    }
 }
 
+main();
+
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
-app.use(express.urlencoded({extended: true}));
+app.set("views", path.join(dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(dirname, "/public")));
 
+app.get("/", (req, res) => {
+    res.redirect("/program");
+});
 
-
-
-app.get("/", (req, res)=>{
-    res.redirect("/program")
-})
-
-
-
-app.get("/program", async(req, res) =>{
+app.get("/program", async (req, res) => {
     const allprograms = await Program.find({});
-    res.render("./programs/index.ejs", {allprograms})
+    res.render("./programs/index.ejs", { allprograms });
 });
 
 app.get("/program/:id", async (req, res) => {
     let { id } = req.params;
-        const program = await Program.findById(id);
-        res.render("./programs/show.ejs", { program});
+    const program = await Program.findById(id);
+    res.render("./programs/show.ejs", { program });
 });
 
 app.get('/program/:id/song/:sectionIndex', async (req, res) => {
     try {
-      const program = await Program.findById(req.params.id);
-      const song = program.sections[req.params.sectionIndex];
-      res.render('./programs/song.ejs', { song, program }); // Render the song page with EJS
+        const program = await Program.findById(req.params.id);
+        const song = program.sections[req.params.sectionIndex];
+        res.render('./programs/song.ejs', { song, program });
     } catch (err) {
-      res.status(500).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }
-  });
-
-
-app.get("/guide", (req, res)=>{
-    res.render("./programs/guide.ejs")
 });
 
-app.get("/me", (req, res)=>{
-    res.render("./programs/me.ejs")
+app.get("/guide", (req, res) => {
+    res.render("./programs/guide.ejs");
 });
 
-app.get("*", (req, res)=>{
-    res.redirect("/program")
-})
-
-app.listen(8080, () =>{
-    console.log("server is listening to port 8080")
+app.get("/me", (req, res) => {
+    res.render("./programs/me.ejs");
 });
 
+app.get("*", (req, res) => {
+    res.redirect("/program");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log("Server is listening to port", (PORT));
+});
